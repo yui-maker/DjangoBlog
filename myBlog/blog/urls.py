7204726017dfda3223a95cdbd7e617ffffd8b018
Priya Singh <priyasinghmech@gmail.com>
2024-07-18 00:04:04 +0300

    proto version working

--- myBlog/blog/urls.py
+++ myBlog/blog/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('wood/', views.wood, name='wood'),
-    path('printing/', views.printing, name='printing'),
-    path('electronics/', views.electronics, name='electronics'),
-    # path('programming/', views.programming, name='programming'),
-    path('others/', views.others, name='others'),
-    # path('about/', views.about, name='about'),
+    path('<str:category>/', views.category_view, name='category'),
 ]
